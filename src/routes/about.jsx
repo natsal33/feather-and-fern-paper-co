8ca03beec6e92f223460624b1f6b22780a8f4ef5
Natalie Salazar <natmsal33@gmail.com>
2023-08-13 23:20:23 -0700

    Basic About and FAQ page outlined

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import "./about.css";
 
 function About() {
-  return <div>This is Hanna's About Page</div>;
+  return (
+    <div id="about-section">
+      <div id="artist-portrait" />
+      <div id="about-commentary">
+        <h2>About Hanna Chee</h2>
+        <p>
+          {" "}
+          Introducing the enigmatic virtuoso of the handwritten word, Sir
+          Penmanship Prestige III, a luminary whose artistry transcends the
+          limits of ink and parchment. Descended from a clandestine lineage of
+          mystical scribes, he began wielding quills before mastering the art of
+          walking. Legend has it that his first word was an impeccable rendition
+          of the Magna Carta.
+        </p>
+        <p>
+          {" "}
+          Prestige's avant-garde approach to calligraphy defies convention.
+          Rumor has it that he communes with ancient spirits of long-lost fonts,
+          channeling their essence into his work. His studio, rumored to be
+          nestled atop a cloud, is said to be a sanctuary of inspiration
+          furnished with levitating inkwells and self-writing parchment.
+        </p>
+        <p>
+          {" "}
+          World leaders and unicorns alike clamor for his inscriptions. He has
+          transcribed sonnets for mermaids and shopping lists for leprechauns.
+          Celestial beings petition him for handwritten horoscopes, delivered
+          via carrier pigeons adorned with quill-plucked feathers.
+        </p>
+        <p>
+          {" "}
+          In a clandestine society of penmen, Sir Penmanship Prestige III
+          remains an elusive luminary, leaving a trail of swooping letters and
+          embellished scrolls in his wake. He swears by an esoteric diet of
+          powdered ink and ambrosia, claiming it fuels his metaphysical
+          connection to calligraphic realms.
+        </p>
+        <p>
+          {" "}
+          Witnessing his craft is said to induce spontaneous rhyming couplets in
+          bystanders. Critics have dubbed his work as "transdimensional
+          graffiti," and connoisseurs insist that owning a piece of his
+          transcendent scribbles bestows eternal eloquence.
+        </p>
+        <p>
+          {" "}
+          In a world awash with digital text, Sir Penmanship Prestige III stands
+          as anachronistic art's last bastion, an enchanter who spins alphabetic
+          spells and conjures meaning with every stroke.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
