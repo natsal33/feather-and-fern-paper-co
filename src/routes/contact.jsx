1ef004beb395e22a1d5dd2738f5bebfe5bbd27a8
Natalie Salazar <natmsal33@gmail.com>
2023-09-04 22:34:12 -0700

    contact form page operational

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -4,37 +4,79 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import "./contact.css";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = React.useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_be0p4oo",
+        "template_mfbxpw6",
+        form.current,
+        "xHUVCzuEFO2xdh_O4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="p-2 mb-4">
       <h2>Want a Consultation?</h2>
-      <Form>
+      <Form ref={form} onSubmit={sendEmail}>
         <div id="inputName">
           <h4>First Name </h4>
-          <Form.Control required></Form.Control>
+          <Form.Control name="first_name" required></Form.Control>
+          <br />
           <h4>Last Name </h4>
-          <Form.Control required></Form.Control>
+          <Form.Control name="last_name" required></Form.Control>
+          <br />
         </div>
-
+        <h4>Email </h4>
+        <Form.Control name="email" required></Form.Control>
         <h4>When is your event?</h4>
-        <Form.Control required type="date"></Form.Control>
+        <Form.Control name="event_date" required type="date"></Form.Control>
         <h4>How many guests are you expecting?</h4>
-        <Form.Control required></Form.Control>
+        <Form.Control name="guest_count" required></Form.Control>
         <h4>Which products are you interested in?</h4>
-        <Form.Check label="Save the Dates/Invitation Suites" />
-        <Form.Check label="Day-Of Paper (signs/menus/seating chart" />
-        <Form.Check label="Gifting (Favors, Welcome Kits, Thank You Cards, Etc.)" />
+        <Form.Check
+          name="std_invitations"
+          label="Save the Dates/Invitation Suites"
+        />
+        <Form.Check
+          name="day_of_paper"
+          label="Day-Of Paper (signs/menus/seating chart"
+        />
+        <Form.Check
+          name="gifting_other"
+          label="Gifting (Favors, Welcome Kits, Thank You Cards, Etc.)"
+        />
         <h4>How did you hear about Hanna Chee Calligraphy?</h4>
-        <Form.Select>
+        <Form.Select name="marketing_source">
           <option value="Friend/Family">Friends/Family</option>
           <option value="Vendor">Other Event Vendor</option>
           <option value="SocialMedia">Instagram/Facebook</option>
         </Form.Select>
         <h4>Any other details you'd like to share?</h4>
-        <Form.Control></Form.Control>
-
-        <Button type="submit">Submit</Button>
+        <Form.Control
+          name="other_details"
+          as="textarea"
+          rows="5"
+          cols="50"
+        ></Form.Control>
+        <br />
+        <Button type="submit" value="Send">
+          Submit
+        </Button>
       </Form>
     </div>
   );
