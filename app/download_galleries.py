3f3cbc931ae231d04841d7db4e1a7bedbd64b22d
Natalie Salazar <natmsal33@gmail.com>
2023-09-23 14:12:11 -0600

    commit before push

--- app/download_galleries.py
+++ app/download_galleries.py
@@ -1,13 +1,19 @@
 import boto3
 import json
+import requests
 
 global AWS_ACCESS_KEY
 global AWS_SECRET_KEY
 global AWS_REGION_NAME
 
-AWS_ACCESS_KEY = ""
-AWS_SECRET_KEY = ""
-AWS_REGION_NAME = "us-west-2"
+response = requests.get(
+    "http://169.254.169.254/latest/meta-data/iam/security-credentials/Feather-and-Fern-EC2"
+)
+credentials = response.json()
+AWS_ACCESS_KEY = credentials["AccessKeyId"]
+AWS_SECRET_KEY = credentials["SecretAccessKey"]
+AWS_REGION_NAME = credentials["Region"]
+AWS_SESSION_TOKEN = credentials["Token"]
 
 
 client = boto3.client(
