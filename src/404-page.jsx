15141dc8dd5c417baa1e8d5e6f89a64bf5c5f890
Natalie Salazar <natmsal33@gmail.com>
2023-11-02 19:40:12 -0700

    Sticky header is functional

--- src/404-page.jsx
+++ src/404-page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./404-page.css";
 
 function Error_404() {
   return (
-    <div>
+    <div id="error-page">
       <h1>404 - Oops!</h1>
       <p>This page does not exist.</p>
+      <p>
+        <a href="/">Click here</a> to return to the Feather & Fern homepage.
+      </p>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,43 @@ import "../theme.css";
 import "./Header.css";
 
 function Header() {
+  const [headerElement, setHeaderElement] = React.useState();
+  const [headerHeight, setHeaderHeight] = React.useState(null);
+  const [windowSize, setWindowSize] = React.useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    if (headerElement != null) {
+      setHeaderHeight(headerElement.getBoundingClientRect().height);
+    }
+    setHeaderElement(document.getElementById("Header"));
+    console.log("Header Height: " + headerHeight);
+
+    // sets windowSize according to event handler that monitors window resizing
+  }, [headerElement, headerHeight, windowSize]);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const headerBoxStyle = {
+    width: "100%",
+    height: headerHeight,
+  };
+
   return (
-    <div className="Header-box">
-      <div className="Header">
+    <div id="header-box" style={headerBoxStyle}>
+      <div id="Header">
         <nav className="navBarSection">
           <NavLink to={"/gallery"} className="navBarItem">
             Gallery
