06456c92c640652639c916daf20eab6fc2877292
Natalie Salazar <natmsal33@gmail.com>
2024-01-04 21:41:30 -0700

    configured Procfile and made style edits to adhere to flask standards (favicon) and made an edit to homepage highlight flex direction.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
-from . import s3_methods
 import re
-import os
 import boto3
-import json
 from flask import Flask, request
 from flask_cors import CORS
+import boto3
 
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
@@ -15,6 +13,34 @@ CORS(app)
 ff_bucket_name = "feather-and-fern-paper-co"
 
 
+s3 = boto3.client("s3")
+
+
+# List objects in bucket with specified prefix
+def list_objects(bucket_name, prefix):
+    response = s3.list_objects(
+        Bucket=bucket_name,
+        Prefix=prefix,
+    )
+    objects = response.get("Contents", [])
+
+    return objects
+
+
+# Upload a file to the bucket
+def upload_file_to_bucket(bucket_name, file_path, object_key):
+    s3.upload_file(file_path, bucket_name, object_key)
+
+
+# Download a file from the bucket
+def download_file_from_bucket(bucket_name, file_path, object_key):
+    s3.download_file(
+        bucket_name,
+        object_key,
+        file_path,
+    )
+
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<string:path>")
 @app.route("/<path:path>")
@@ -22,24 +48,16 @@ def index(path):
     return app.send_static_file("index.html")
 
 
-@app.route("/database")
-def connected_to_database():
-    print("connected to database")
-    return "CONNECTED"
-
-
 # add try catch statement if arguments are not passed
 @app.route("/database/fetch-gallery")
 def get_gallery_photos():
     if request.args:
         gallery_name = request.args.get("album-name", None)
-        print("gallery name: " + gallery_name)
         gallery_photo_array = []
-        photos = s3_methods.list_objects(ff_bucket_name, "gallery/" + gallery_name)
+        photos = list_objects(ff_bucket_name, "gallery/" + gallery_name)
 
         for photo in photos:
             photo_name = photo["Key"].removeprefix("gallery/" + gallery_name + "/")
-            print("photo name: " + photo_name)
             if photo_name != "":
                 photo_S3_url = f"https://feather-and-fern-paper-co.s3.us-west-2.amazonaws.com/gallery/{gallery_name}/{photo_name}"
                 photo_dict = {"name": photo_name, "url": photo_S3_url}
@@ -52,7 +70,7 @@ def get_gallery_photos():
 def get_gallery_thumbnails():
     gallery_thumbnail_array = []
 
-    photos = s3_methods.list_objects(ff_bucket_name, "")
+    photos = list_objects(ff_bucket_name, "")
 
     for photo in photos:
         photo_path = photo["Key"]
